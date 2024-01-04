1a9da8b440305dfe18e4e9466abafb79a994b52d
hkey <81494947+hkey0@users.noreply.github.com>
2024-01-04 18:48:45 +0300

    Update decorators.py

--- pinksale/decorators.py
+++ pinksale/decorators.py
@@ -1,4 +1,4 @@
-def needs_private_key(function):
+def needs_private_key(function) -> None:
     def wrapper(*args, **kwargs):
         assert args[0].private_key != None, "This functions need a private key."
         resp = function(*args, **kwargs)
